463e5b0870403e61c95cebe1fc8c5cb1a75215da
soniya437 <ysoniya437@gmail.com>
2022-12-18 21:32:35 +0530

    app created

---
+++ chat-app/nodeServer/index.js
@@ -0,0 +1,35 @@
+//Node server which will handle socket io connections.
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server) //port=8000
+
+const users = {}
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        //console.log("new user", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left',  users[socket.id])
+        delete users[socket.id];
+    });
+
+})
+
+server.prependListener("request", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+ });
+
+ server.listen(8000,()=>{
+    console.log('This is the socket server')
+ })
+
