03d4aac1c2f9809f13021089a66e92caaca26967
soniya437 <ysoniya437@gmail.com>
2022-12-18 23:47:21 +0530

    updated

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,23 +1,27 @@
-//Node server which will handle socket io connections.
+
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app);
 
+//Node server which will handle socket io connections.
 const io = require('socket.io')(server) //port=8000
 
 const users = {}
 
+// if any new user connects, inform to all
 io.on('connection', socket => {
     socket.on('new-user-joined', name => {
-        //console.log("new user", name);
+        
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
+    //if someone send message, broadcast that to all
     socket.on('send', message => {
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
     });
-
+    
+    //if someone leaves, inform to all
     socket.on('disconnect', message => {
         socket.broadcast.emit('left',  users[socket.id])
         delete users[socket.id];
